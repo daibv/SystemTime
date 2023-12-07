446987aa6098546887e19e5c1afe76bb600bdad1
daibv <152725726+daibv@users.noreply.github.com>
2023-12-07 17:00:16 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SystemTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            System.Console.WriteLine("DateTime Now is:" + localDate);
+        }
+    }
+}
